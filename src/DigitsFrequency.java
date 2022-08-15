0b5b380fb54185e29e96d32bc60d85a31bfe63a8
Amit <amitkr24031995@gmail.com>
2022-08-15 18:36:45 +0530

    Till15August1

--- src/DigitsFrequency.java
+++ src/DigitsFrequency.java
@@ -1,2 +1,8 @@
+import java.util.Scanner;
+
 public class DigitsFrequency {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    }
 }
